c1c22ee62767e4a0e7435e6766cb85aba7422007
Richard Ochei <162317835+OxheiCodes@users.noreply.github.com>
2024-03-21 14:36:10 +0000

    Logbook 4 need finishing

--- Logbook 4/confirmation.php
+++ Logbook 4/confirmation.php
@@ -1,17 +1,30 @@
 <?php
-	//NOTE you must also START sessions where they are used as well as
-	//where they are created
+session_start();
 
-	session_start();
+$qty = $_SESSION['selqty'];
+$size = $_SESSION['selsize'];
+$colour = $_POST['selcolour'];
+$price = 0;
 
-	//NOTE: A new variable created to ensure that prices are to 2 dp
-	$finalPrice = number_format($_SESSION['quantity']*$_SESSION['price'], 2);
-	//NOTE that unlike $_POST[], it seems $_SESSION[] must be concatenated to text output
+switch ($size) {
+    case "Small":
+        $price = 15.75;
+        break;
+    case "Medium":
+        $price = 16.75;
+        break;
+    case "Large":
+        $price = 17.75;
+        break;
+    case "ExtraLarge":
+        $price = 18.75;
+        break;
+}
 
-	echo "<h2> Your order quantity is     : ". $_SESSION['quantity']. "</h2,><br/>";
-	echo "<h2> The price of widgets is    : �". $_SESSION['price']. "</h2,><br/>";
-   	echo "<h2> and the selected colour is : $_POST[selcolour]</h2><br/><br/>";
-  	echo "<h2> Total price to pay is      : �". $finalPrice."</h2,><br/>";
+$total_cost = $qty * $price;
 
+echo "<h2>Your order quantity is $qty</h2>";
+echo "<h2>Selected size: $size (£$price)</h2>";
+echo "<h2>Selected colour: $colour</h2>";
+echo "<h2>Total cost: £$total_cost</h2>";
 ?>
-
--- Logbook 4/size.php
+++ Logbook 4/size.php
@@ -1,12 +1,20 @@
 <?php
 session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $_SESSION['qty'] = $_POST['qty'];
-    $_SESSION['size'] = $_POST['size'];
-}
-
-header("Location: selectcolour.php");
-exit;
+$_SESSION['selqty'] = $_POST['selqty'];
 ?>
-
+<html>
+<head>
+    <title>Size Selection Page</title>
+</head>
+<body>
+    <form action="selectcolour.php"  method="post">
+        Select the size for the <?php echo $_SESSION['selqty'] ?> widgets you are ordering:<br>
+        <input type="radio" name="selsize" value="Small"> Small (£15.75)<br>
+        <input type="radio" name="selsize" value="Medium"> Medium (£16.75)<br>
+        <input type="radio" name="selsize" value="Large"> Large (£17.75)<br>
+        <input type="radio" name="selsize" value="ExtraLarge"> Extra Large (£18.75)<br>
+        <br/><br/>
+        <input type="submit" value="Proceed to Colour Selection"/>
+    </form>
+</body>
+</html>
